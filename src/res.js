eb8ffe0e4314d64fe0a443f8f925fa7e5138f192
Tintin Ho <holoktin97@gmail.com>
2016-08-30 00:13:37 +0800

    Add Kono singleplayer game

---
+++ src/res.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    images: [
+        "assets/images/redIcon.png",
+        "assets/images/redIconEm.png",
+        "assets/images/blueIcon.png",
+        "assets/images/blueIconEm.png",
+        "assets/images/board.png",
+        "assets/images/panel.png",
+        "assets/images/blueTile.png",
+        "assets/images/blueTileEm.png",
+        "assets/images/redTile.png",
+        "assets/images/redTileEm.png",
+        "assets/images/emptyTile.png",
+        "assets/images/emptyTileEm.png",
+    ],
+};
